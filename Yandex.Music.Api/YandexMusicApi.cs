ba693ae6247bcdb9dfa0b8fe55aad146653a7daa
Winster332 <roof.is.on.fire.science@gmail.com>
2020-01-14 09:12:06 +0300

    created function for remove playlist by kind

--- Yandex.Music.Api/YandexMusicApi.cs
+++ Yandex.Music.Api/YandexMusicApi.cs
@@ -907,6 +907,50 @@ namespace Yandex.Music.Api
       };
     }
 
+    public bool RemovePlaylist(long kind)
+    {
+      try
+      {
+        var accounts = GetAccounts();
+        var auth = GetAuth(accounts.DefaultUID);
+
+        var uri = new Uri("https://music.yandex.ru/handlers/change-playlist.jsx");
+        var request = GetRequest(uri,
+          new KeyValuePair<string, string>("action", "delete"),
+          new KeyValuePair<string, string>("kind", kind.ToString()),
+          new KeyValuePair<string, string>("lang", "ru"),
+          new KeyValuePair<string, string>("sign", auth.User.Sign),
+          new KeyValuePair<string, string>("experiments", auth.Experiments),
+          new KeyValuePair<string, string>("external-domain", "music.yandex.ru"),
+          new KeyValuePair<string, string>("overembed", "false")
+        );
+        request.Headers[HttpRequestHeader.Accept] = "application/json, text/javascript, */*; q=0.01";
+        request.Headers["Accept-Encoding"] = "gzip, deflate, br";
+        request.Headers["Accept-Language"] = "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7";
+        request.Headers["access-control-allow-methods"] = "[POST]";
+        request.Headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8";
+
+        request.Headers["X-Current-UID"] = accounts.DefaultUID;
+        request.Headers["X-Requested-With"] = "XMLHttpRequest";
+        request.Headers["X-Retpath-Y"] = "https%3A%2F%2Fmusic.yandex.ru%2Fusers%2FWinster332%2Fplaylists";
+
+        request.Headers["origin"] = "https://music.yandex.ru";
+        request.Headers["referer"] = "https://music.yandex.ru/users/Winster332/playlists";
+        request.Headers["sec-fetch-mode"] = "cors";
+        request.Headers["sec-fetch-site"] = "same-site";
+        
+        request.GetResponse();
+        
+        return true;
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex);
+      }
+
+      return false;
+    }
+
     public YandexGetCookieResult GetYandexCookie()
     {
       try
