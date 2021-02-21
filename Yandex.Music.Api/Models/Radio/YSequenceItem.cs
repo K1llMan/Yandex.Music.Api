9a88ca0b5604f1bc725855c533933304d1b906ef
K1llM@n <k1llmanws@yandex.ru>
2021-02-21 19:03:58 +0300

    Добавлена модель YTrackParameters

--- Yandex.Music.Api/Models/Radio/YSequenceItem.cs
+++ Yandex.Music.Api/Models/Radio/YSequenceItem.cs
@@ -6,6 +6,7 @@ namespace Yandex.Music.Api.Models.Radio
     {
         public bool Liked { get; set; }
         public YTrack Track { get; set; }
+        public YTrackParameters TrackParameters { get; set; }
         public string Type { get; set; }
     }
 }
---
+++ Yandex.Music.Api/Models/Radio/YTrackParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace Yandex.Music.Api.Models.Radio
+{
+    public class YTrackParameters
+    {
+        public int Bpm { get; set; }
+        public int Hue { get; set; }
+        public decimal Energy { get; set; }
+    }
+}
