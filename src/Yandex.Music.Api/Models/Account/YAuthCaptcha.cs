21e8fa0ace32857f4c4a859a3b8eddc6b44f5b77
Evgeny <mail4evgeniy@gmail.com>
2023-01-01 23:28:17 +0300

    refactoring

--- src/Yandex.Music.Api/Models/Account/YAuthCaptcha.cs
+++ src/Yandex.Music.Api/Models/Account/YAuthCaptcha.cs
@@ -2,14 +2,6 @@
 
 namespace Yandex.Music.Api.Models.Account
 {
-    public class YAuthCaptchaVoice
-    {
-        public string Url { get; set; }
-
-        [JsonProperty("intro_url")]
-        public string IntroUrl { get; set; }
-    }
-
     public class YAuthCaptcha : YAuthBase
     {
         public string Id { get; set; }
---
+++ src/Yandex.Music.Api/Models/Account/YAuthCaptchaVoice.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Yandex.Music.Api.Models.Account
+{
+    public class YAuthCaptchaVoice
+    {
+        public string Url { get; set; }
+
+        [JsonProperty("intro_url")]
+        public string IntroUrl { get; set; }
+    }
+}
