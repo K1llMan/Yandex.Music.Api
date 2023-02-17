4e2aeb3390ba97043c0167bd9819c285d957cc82
K1llM@n <k1llmanws@yandex.ru>
2023-02-17 18:08:30 +0300

    Обновлены модели

--- src/Yandex.Music.Api/Models/Radio/YStationSequence.cs
+++ src/Yandex.Music.Api/Models/Radio/YStationSequence.cs
@@ -7,6 +7,7 @@ namespace Yandex.Music.Api.Models.Radio
         public string BatchId { get; set; }
         public YStationId Id { get; set; }
         public bool Pumpkin { get; set; }
+        public string RadioSessionId { get; set; }
         public List<YSequenceItem> Sequence { get; set; }
     }
 }
