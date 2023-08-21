1d021b21ad13fec20e4cbfb4cae1a5733995edd3
K1llM@n <k1llmanws@yandex.ru>
2023-08-21 20:31:10 +0300

    Обновлены модели.

--- src/Yandex.Music.Api/Models/Landing/YLandingBlock.cs
+++ src/Yandex.Music.Api/Models/Landing/YLandingBlock.cs
@@ -9,10 +9,13 @@ namespace Yandex.Music.Api.Models.Landing
     public class YLandingBlock
     {
         public string Id { get; set; }
+        public string BackgroundImageUrl { get; set; }
+        public string BackgroundVideoUrl { get; set; }
         public YLandingBlockData Data { get; set; }
         public string Description { get; set; }
         [JsonConverter(typeof(YLandingEntityConverter))]
         public List<YLandingEntity> Entities { get; set; }
+        public YLandingBlockPlayContext PlayContext { get; set; }
         public string Title { get; set; }
         public YLandingBlockType Type { get; set; }
         public string TypeForFrom { get; set; }
