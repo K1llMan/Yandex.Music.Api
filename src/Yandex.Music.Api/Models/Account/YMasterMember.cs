edd5dd967fcc67f6f7f265c81c76c93792065835
martin211 <mail4evgeniy@gmail.com>
2026-03-18 23:58:52 +0300

    Новый Passport API

--- src/Yandex.Music.Api/Models/Account/YMasterMember.cs
+++ src/Yandex.Music.Api/Models/Account/YMasterMember.cs
@@ -1,27 +1,28 @@
 ﻿using Newtonsoft.Json;
 
-namespace Yandex.Music.Api.Models.Account;
-
-public class YMasterMember
+namespace Yandex.Music.Api.Models.Account
 {
-    [JsonProperty("uid")]
-    public int Uid { get; set; }
-    
-    [JsonProperty("display_login")]
-    public string DisplayLogin { get; set; }
+    public class YMasterMember
+    {
+        [JsonProperty("uid")]
+        public int Uid { get; set; }
 
-    [JsonProperty("public_name")]
-    public string PublicName { get; set; }
+        [JsonProperty("display_login")]
+        public string DisplayLogin { get; set; }
 
-    [JsonProperty("avatar_url")]
-    public string AvatarUrl { get; set; }
+        [JsonProperty("public_name")]
+        public string PublicName { get; set; }
 
-    [JsonProperty("has_plus")] 
-    public bool HasPlus { get; set; }
+        [JsonProperty("avatar_url")]
+        public string AvatarUrl { get; set; }
 
-    [JsonProperty("Secure_phone_number")]
-    public string SecurePhoneNumber { get; set; }
+        [JsonProperty("has_plus")]
+        public bool HasPlus { get; set; }
 
-    [JsonProperty("primary_alias_type")] 
-    public int PrimaryAliasType { get; set; }
+        [JsonProperty("Secure_phone_number")]
+        public string SecurePhoneNumber { get; set; }
+
+        [JsonProperty("primary_alias_type")]
+        public int PrimaryAliasType { get; set; }
+    }
 }
